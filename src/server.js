0f24959aa63ef326870a1c82ca10dd17ba106a92
Gheerwijn Clicque <Gheerwijn@MBP-van-Gheerwijn.local>
2016-07-09 22:34:40 +0200

    sqlite3

--- src/server.js
+++ src/server.js
@@ -2,21 +2,45 @@ var express = require('express');
 var app = express();
 var config = require('./config')();
 var http = require('http');
+var bodyParser = require('body-parser');
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('filmomat');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 console.log(__dirname + '\\css');
 
 app.get('/', function(req, res) {
+	db.serialize(function() {
+		db.each("SELECT film_name as filmname, iso, manufacturer, processid, process_name as processname, step_id, step_name as step, step_time as time, temp, interval, chemical, dilution FROM FILMS INNER JOIN PROCESSES ON id = film_id INNER JOIN STEPS ON process_id = process_id where film_id = 1", function(error, row) {
+			console.log(row.filmname);
+		});
+
+	});
+
+
 	res.render('index', {});
 });
 
 app.get('/newfilm', function(req, res) {
-	res.render('addfilm', {});
+	res.render('newfilm', {});
+});
+
+app.post('/newfilm', function(req, res) {
+	var name = req.body.name;
+	var iso = req.body.iso;
+	var manufacturer = req.body.manufacturer;
+
+	console.log("name: " + name + " ,iso: " + iso + " ,manufacturer: " + manufacturer);
+
+
+
 });
 
 var server = app.listen(config.port, function() {
