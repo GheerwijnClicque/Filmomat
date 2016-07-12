6b8cb8b9fb4cce018c333186864a6a294ac45c03
Gheerwijn Clicque <Gheerwijn@MBP-van-Gheerwijn.local>
2016-07-12 21:41:41 +0200

    new process implementation

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -6,12 +6,22 @@ $(function() {
     });
 
     $('#newstep').on('click', function() {
-        console.log("new step");
-        $('#steps').append("<ul class=\"step\">    <div id=\"left\"> <li><p>Name:</p><input name=\"name\" type=\"text\"/></li><li><p>Duration:</p><input name=\"name\" type=\"text\"/></li><li><p>Temperature:</p><input name=\"name\" type=\"text\"/></div>  <div id=\"right\"> </li><li><p>Interval:</p><input name=\"name\" type=\"text\"/></li><li><p>Chemical:</p><input name=\"name\" type=\"text\"/></li><li><p>Dilution:</p><input name=\"name\" type=\"text\"/></li></div>  </ul>");
+        $('#steps table').append('<tr class="step"><td><input type="text" value="" placeholder="Developing" id="name"/></td><td><input type="time" value="" id="duration"/></td><td><input type="number" value="" placeholder="21" id="temperature"/></td><td><input type="time" value="" id="interval"/></td><td><input type="text" value="" placeholder="T-max developer" id="chemical"/></td><td><input type="text" value="" placeholder="1:4" id="dilution"/></td><td><a href="#">Delete</a></td></tr>');
     });
 
+    $('#submit').on('click', function(e) {
+        e.preventDefault();
+        $('.step').each(function() {
+            $('td input').each(function() {
+                console.log($(this).val());
+            });
+        });
 
 
+
+    });
+
+// \"
 
 
 
