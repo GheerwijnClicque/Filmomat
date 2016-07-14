0a5171d95aeaf687da1a90cd6cee002728eab0e0
Gheerwijn Clicque <Gheerwijn@MBP-van-Gheerwijn.local>
2016-07-14 09:03:18 +0200

    fixed add new film problems

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -9,7 +9,7 @@ $(function() {
         $('#steps table').append('<tr class="step"><td><input type="text" value="" placeholder="Developing" id="name"/></td><td><input type="time" value="" id="duration"/></td><td><input type="number" value="" placeholder="21" id="temperature"/></td><td><input type="time" value="" id="interval"/></td><td><select class="chemical"><option value="A">A</option><option value="B">B</option><option value="C">C</option><option value="water">Water</option></select></td><td><input type="text" value="" placeholder="1:4" id="dilution"/></td><td><a href="#">Delete</a></td></tr>');
     });
 
-    $('#submit').on('click', function(e) {
+    $('#submitProcess').on('click', function(e) {
         e.preventDefault();
         var steps = [];
         $('.step').each(function() {
